441b743e07a058f05ea3e79476376d6749a97fc9
DenysKors <105918404+DenysKors@users.noreply.github.com>
2023-02-21 16:23:56 +0200

    Hide/Show Keyboard

--- my-app/Screens/Components/RegistrationScreen/RegistrationScreen.js
+++ my-app/Screens/Components/RegistrationScreen/RegistrationScreen.js
@@ -1,33 +1,83 @@
-import { StyleSheet, Text, TextInput, View, TouchableOpacity } from "react-native";
+import { useState } from "react";
+import {
+	StyleSheet,
+	ImageBackground,
+	Text,
+	TextInput,
+	View,
+	TouchableOpacity,
+	Platform,
+	KeyboardAvoidingView,
+	TouchableWithoutFeedback,
+	Keyboard,
+} from "react-native";
 
 export const RegistrationScreen = () => {
+	const [isShowKeyboard, setIsShowKeyboard] = useState(false);
+
+	const keyboardHide = () => {
+		setIsShowKeyboard(false);
+		Keyboard.dismiss();
+	};
+
 	return (
-		<View style={styles.box}>
-			<Text style={styles.formTitle}>Регистрация</Text>
-			<View style={styles.form}>
-				<View style={{ marginBottom: 16 }}>
-					<TextInput style={styles.input} placeholder={"Логин"} placeholderTextColor={"#BDBDBD"} />
-				</View>
-				<View style={{ marginBottom: 16 }}>
-					<TextInput style={styles.input} placeholder={"Адрес электронной почты"} placeholderTextColor={"#BDBDBD"} />
-				</View>
-				<View style={{ marginBottom: 43 }}>
-					<TextInput
-						style={styles.input}
-						placeholder={"Пароль"}
-						placeholderTextColor={"#BDBDBD"}
-						secureTextEntry={true}
-					/>
-				</View>
-				<TouchableOpacity activeOpacity={0.8} style={styles.btn}>
-					<Text style={styles.btnTitle}>Зарегистрироваться</Text>
-				</TouchableOpacity>
-			</View>
+		<View style={styles.container}>
+			<TouchableWithoutFeedback onPress={keyboardHide}>
+				<ImageBackground style={styles.image} source={require("../../../assets/images/background-image.jpg")}>
+					<KeyboardAvoidingView behavior={Platform.OS === "ios" && "padding"}>
+						<View style={{ ...styles.box, paddingBottom: isShowKeyboard ? 32 : 45 }}>
+							<Text style={styles.formTitle}>Регистрация</Text>
+							<View style={styles.form}>
+								<View style={{ marginBottom: 16 }}>
+									<TextInput
+										style={styles.input}
+										placeholder={"Логин"}
+										placeholderTextColor={"#BDBDBD"}
+										onFocus={() => setIsShowKeyboard(true)}
+									/>
+								</View>
+								<View style={{ marginBottom: 16 }}>
+									<TextInput
+										style={styles.input}
+										placeholder={"Адрес электронной почты"}
+										placeholderTextColor={"#BDBDBD"}
+										onFocus={() => setIsShowKeyboard(true)}
+									/>
+								</View>
+								<View style={{ marginBottom: 43 }}>
+									<TextInput
+										style={styles.input}
+										placeholder={"Пароль"}
+										placeholderTextColor={"#BDBDBD"}
+										secureTextEntry={true}
+										onFocus={() => setIsShowKeyboard(true)}
+									/>
+								</View>
+								<TouchableOpacity activeOpacity={0.8} style={styles.btnRegister}>
+									<Text style={styles.btnRegisterTitle}>Зарегистрироваться</Text>
+								</TouchableOpacity>
+							</View>
+							<TouchableOpacity style={styles.btnLogin}>
+								<Text style={styles.btnLoginTitle}>Уже есть аккаунт? Войти</Text>
+							</TouchableOpacity>
+						</View>
+					</KeyboardAvoidingView>
+				</ImageBackground>
+			</TouchableWithoutFeedback>
 		</View>
 	);
 };
 
 const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+	},
+	image: {
+		flex: 1,
+		resizeMode: "cover",
+		justifyContent: "flex-end",
+	},
 	box: {
 		paddingTop: 92,
 		borderTopRightRadius: 25,
@@ -35,7 +85,12 @@ const styles = StyleSheet.create({
 
 		backgroundColor: "#FFF",
 	},
-	formTitle: { marginBottom: 33, fontSize: 30, textAlign: "center", color: "#212121" },
+	formTitle: {
+		marginBottom: 33,
+		fontSize: 30,
+		textAlign: "center",
+		color: "#212121",
+	},
 	form: {
 		marginHorizontal: 16,
 		backgroundColor: "#FFF",
@@ -55,7 +110,8 @@ const styles = StyleSheet.create({
 		backgroundColor: "#F6F6F6",
 		color: "#212121",
 	},
-	btn: {
+	btnRegister: {
+		marginBottom: 16,
 		height: 50,
 
 		justifyContent: "center",
@@ -64,10 +120,22 @@ const styles = StyleSheet.create({
 		borderRadius: 50,
 		backgroundColor: "#FF6C00",
 	},
-	btnTitle: {
+	btnRegisterTitle: {
 		fontSize: 16,
 		lineHeight: 19,
 
 		color: "#FFF",
+	},
+	btnLogin: {
+		marginHorizontal: 50,
+
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	btnLoginTitle: {
+		fontSize: 16,
+		lineHeight: 19,
+
+		color: "#1B4371",
 	},
 });
