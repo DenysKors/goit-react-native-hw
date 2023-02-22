44a9b87e26f829bd84d7eecada8ba4aacfe84147
DenysKors <105918404+DenysKors@users.noreply.github.com>
2023-02-22 13:09:13 +0200

    Registration/Login styles and logic

--- my-app/Screens/Components/RegistrationScreen/RegistrationScreen.js
+++ my-app/Screens/Components/RegistrationScreen/RegistrationScreen.js
@@ -1,4 +1,4 @@
-import { useState, useCallback } from "react";
+import { useState, useCallback, useEffect } from "react";
 import {
 	StyleSheet,
 	ImageBackground,
@@ -10,10 +10,12 @@ import {
 	KeyboardAvoidingView,
 	TouchableWithoutFeedback,
 	Keyboard,
+	Dimensions,
 } from "react-native";
 
 import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
+import AddImage from "../../../assets/images/add.svg";
 
 SplashScreen.preventAutoHideAsync();
 
@@ -22,7 +24,24 @@ export const RegistrationScreen = () => {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
 
+	const [loginFocused, setLoginFocused] = useState("");
+	const [emailFocused, setEmailFocused] = useState(false);
+	const [paswordFocused, setPaswordFocused] = useState(false);
+
 	const [isShowKeyboard, setIsShowKeyboard] = useState(false);
+
+	const [dimensions, setDimensions] = useState(Dimensions.get("window").width - 16 * 2);
+
+	useEffect(() => {
+		const onChange = () => {
+			const width = Dimensions.get("window").width - 16 * 2;
+			setDimensions(width);
+		};
+		const dimentionListener = Dimensions.addEventListener("change", onChange);
+		return () => {
+			dimentionListener.remove();
+		};
+	}, []);
 
 	const keyboardHide = () => {
 		setIsShowKeyboard(false);
@@ -30,6 +49,9 @@ export const RegistrationScreen = () => {
 	};
 
 	const onSubmitForm = () => {
+		if (login === "" || email === "" || password === "") {
+			return alert("Please fill all fields");
+		}
 		setIsShowKeyboard(false);
 		Keyboard.dismiss();
 		setLogin("");
@@ -59,37 +81,65 @@ export const RegistrationScreen = () => {
 				<ImageBackground style={styles.image} source={require("../../../assets/images/background-image.jpg")}>
 					<KeyboardAvoidingView behavior={Platform.OS === "ios" && "padding"}>
 						<View style={{ ...styles.box, paddingBottom: isShowKeyboard ? 32 : 45 }}>
+							<View style={styles.userImage}></View>
+							<TouchableOpacity style={styles.addImageBtn}>
+								<AddImage />
+							</TouchableOpacity>
 							<Text style={styles.formTitle}>Регистрация</Text>
-							<View style={styles.form}>
+							<View style={{ ...styles.form, width: dimensions }}>
 								<View style={{ marginBottom: 16 }}>
 									<TextInput
-										style={styles.input}
+										style={{
+											...styles.input,
+											borderColor: loginFocused ? "#FF6C00" : "#E8E8E8",
+											backgroundColor: loginFocused ? "#FFF" : "#F6F6F6",
+										}}
 										placeholder={"Логин"}
 										placeholderTextColor={"#BDBDBD"}
 										value={login}
 										onChangeText={value => setLogin(value)}
-										onFocus={() => setIsShowKeyboard(true)}
+										onFocus={() => {
+											setIsShowKeyboard(true);
+											setLoginFocused(true);
+										}}
+										onBlur={() => setLoginFocused(false)}
 									/>
 								</View>
 								<View style={{ marginBottom: 16 }}>
 									<TextInput
-										style={styles.input}
+										style={{
+											...styles.input,
+											borderColor: emailFocused ? "#FF6C00" : "#E8E8E8",
+											backgroundColor: emailFocused ? "#FFF" : "#F6F6F6",
+										}}
 										placeholder={"Адрес электронной почты"}
 										placeholderTextColor={"#BDBDBD"}
 										value={email}
 										onChangeText={value => setEmail(value)}
-										onFocus={() => setIsShowKeyboard(true)}
+										onFocus={() => {
+											setIsShowKeyboard(true);
+											setEmailFocused(true);
+										}}
+										onBlur={() => setEmailFocused(false)}
 									/>
 								</View>
 								<View style={{ marginBottom: 43 }}>
 									<TextInput
-										style={styles.input}
+										style={{
+											...styles.input,
+											borderColor: paswordFocused ? "#FF6C00" : "#E8E8E8",
+											backgroundColor: paswordFocused ? "#FFF" : "#F6F6F6",
+										}}
 										placeholder={"Пароль"}
 										placeholderTextColor={"#BDBDBD"}
 										secureTextEntry={true}
 										value={password}
 										onChangeText={value => setPassword(value)}
-										onFocus={() => setIsShowKeyboard(true)}
+										onFocus={() => {
+											setIsShowKeyboard(true);
+											setPaswordFocused(true);
+										}}
+										onBlur={() => setPaswordFocused(false)}
 									/>
 								</View>
 								<TouchableOpacity activeOpacity={0.8} style={styles.btnRegister} onPress={onSubmitForm}>
@@ -124,7 +174,24 @@ const styles = StyleSheet.create({
 		borderTopRightRadius: 25,
 		borderTopLeftRadius: 25,
 
+		alignItems: "center",
+
 		backgroundColor: "#FFF",
+	},
+	userImage: {
+		width: 120,
+		height: 120,
+
+		position: "absolute",
+		top: -60,
+
+		backgroundColor: "#F6F6F6",
+		borderRadius: 16,
+	},
+	addImageBtn: {
+		position: "absolute",
+		top: 20,
+		right: 125,
 	},
 	formTitle: {
 		marginBottom: 33,
@@ -134,7 +201,6 @@ const styles = StyleSheet.create({
 		color: "#212121",
 	},
 	form: {
-		marginHorizontal: 16,
 		backgroundColor: "#FFF",
 	},
 	input: {
